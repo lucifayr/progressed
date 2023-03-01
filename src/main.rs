f5a15f9237726ff25b9a977b4f7ae1408d84d7bb
jackboxx <lucygschwantner@gmail.com>
2023-03-01 23:09:05 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{thread, time::Duration};
 use progressive::{bar::progress_bar::ProgressBar, style::bar_style::ProgressBarStyle};
 
 fn main() {
+    println!("Starting progress bar 1...");
     for _ in ProgressBar::new(0..50)
         .set_style(ProgressBarStyle::smooth())
         .set_title("progress bar: ")
@@ -10,6 +11,7 @@ fn main() {
         thread::sleep(Duration::from_millis(50));
     }
 
+    println!("Starting progress bar 2...");
     for _ in ProgressBar::new(0..50)
         .set_style(ProgressBarStyle::arch())
         .set_title("progress bar 2: ")
@@ -17,6 +19,7 @@ fn main() {
         thread::sleep(Duration::from_millis(50));
     }
 
+    println!("Starting progress bar 3...");
     for _ in ProgressBar::new(0..50)
         .set_style(ProgressBarStyle::default())
         .set_title("progress bar 3: ")
@@ -24,6 +27,7 @@ fn main() {
         thread::sleep(Duration::from_millis(50));
     }
 
+    println!("Starting progress bar 4...");
     for _ in ProgressBar::new(0..50)
         .set_style(ProgressBarStyle::cargo())
         .set_title("progress bar 4: ")
